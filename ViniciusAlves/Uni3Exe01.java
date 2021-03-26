03fae2b59eb7fc9df89470e868908ed37d505fae
Vinicius Alves <viniciusalvesdeoliveira@hotmail.com>
2021-03-25 23:25:09 -0300

    feat: Uni3Exe01

--- ViniciusAlves/Uni3Exe01.java
+++ ViniciusAlves/Uni3Exe01.java
@@ -4,10 +4,10 @@ public class Uni3Exe01 {
     public static void main(String[] args) throws Exception {
         Scanner teclado = new Scanner(System.in);
  
-        float base = teclado.nextFloat();
-        float altura = teclado.nextFloat();
+        float tamanho1 = teclado.nextFloat();
+        float tamanho2 = teclado.nextFloat();
  
-        System.out.println("Area do terreno: " + (base * altura));
+        System.out.println("Area do terreno: " + (tamanho1 * tamanho2));
  
         teclado.close();
     }
