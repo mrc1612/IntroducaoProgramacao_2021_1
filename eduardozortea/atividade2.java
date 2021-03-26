b519af502cd13ed355b292432a1efe2ed1825cbf
EduardoZortea18 <60518656+EduardoZortea18@users.noreply.github.com>
2021-03-26 18:50:26 -0300

    atividade2

---
+++ eduardozortea/atividade2.java
@@ -0,0 +1,16 @@
+package eduardozortea;
+
+import java.util.Scanner;
+
+public class atividade2 {
+    public static void main(String[] args) {
+        Scanner scanner = new Scanner(System.in);
+        
+        double valor = scanner.nextDouble();
+
+        double desconto = valor * 0.12;
+
+        System.out.println("Valor do desconto: " + desconto);
+        System.out.println("Produto com desconto: " + (valor - desconto));
+    }
+}
