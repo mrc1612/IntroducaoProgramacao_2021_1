aa589fbb4cf2de9ee918fb3042cf6d4fed7bb562
EduardoZortea18 <60518656+EduardoZortea18@users.noreply.github.com>
2021-03-26 19:22:55 -0300

    atividade5

---
+++ eduardozortea/atividade5.java
@@ -0,0 +1,13 @@
+package eduardozortea;
+
+import java.util.Scanner;
+
+public class atividade5 {
+    public static void main(String[] args) {
+        Scanner scanner = new Scanner(System.in);
+        int qtdFrango = scanner.nextInt();
+
+        System.out.println("Gasto por frango: " + ((4.00)+(3.50*2)));
+        System.out.println("gasto total: " + ((4.00 + 3.50*2)*qtdFrango));
+    }
+}
