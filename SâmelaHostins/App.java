94712c0ecff304c2fc0adec0d46ee8ec787d7709
Sâmela Hostins <samela@hostins.com.br>
2021-03-26 19:08:49 -0300

    Add files via upload

--- SâmelaHostins/App.java
+++ SâmelaHostins/App.java
@@ -1,14 +1,14 @@
 import java.util.Scanner;
-
 public class App {
     public static void main(String[] args) throws Exception {
         Scanner ler = new Scanner(System.in);
-        int largura, comprimento, area;
-        System.out.println("Informe a largura do terreno");
-        largura = ler.nextInt();
-        System.out.println("Informe o comprimento do terreno");
-        comprimento = ler.nextInt();
-        area = (comprimento * largura);
-        System.out.println("A área desse terreno é: " + area);
+        double desconto, preco, valorf;
+        double porcentagem = 0.12;
+        System.out.println("Informe o preço do calçado");
+        preco = ler.nextInt();
+        desconto = (preco * porcentagem);
+        valorf = (preco - desconto);
+        System.out.println("O valor do desconto é de: " + desconto);
+        System.out.println("O preço do par de sapatos com desconto é R$ " + valorf);
     }
 }
