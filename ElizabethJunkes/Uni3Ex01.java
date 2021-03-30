d67df7d1da26922de3ffe941ec74ef3811e2b21a
ElizabethJunkes <81395539+ElizabethJunkes@users.noreply.github.com>
2021-03-30 19:06:13 -0300

    Add files via upload

---
+++ ElizabethJunkes/Uni3Ex01.java
@@ -0,0 +1,14 @@
+package uni3ex01;
+import java.util.*;
+
+public class Uni3Ex01 {
+    public static void main(String[] args) {
+        Scanner teclado = new Scanner(System.in);
+        System.out.print("Base do terreno: ");
+        double retangulo_base = teclado.nextDouble();
+        System.out.print("Altura do terreno: ");
+        double retangulo_altura = teclado.nextDouble();
+        System.out.println("Área total do terreno: " + (retangulo_base * retangulo_altura));
+    }
+}
+   
