df603dd5d4137b980bb4813916e9eff38d3616f2
MurakamiJP <56041365+MurakamiJP@users.noreply.github.com>
2021-04-05 12:43:22 -0300

    Add files via upload

---
+++ MurakamiJP/Exercicio6.java
@@ -0,0 +1,19 @@
+import java.util.Scanner;
+
+public class Exercicio6 {
+
+    public static void main(String[] args) throws Exception {
+
+        Scanner teclado = new Scanner(System.in);
+        
+        System.out.print("Restaurante e Churrascaria Do Japa $ 25 QUILO $ ");
+        System.out.print("\n");
+        
+        System.out.print("Informe o peso do prato montado do cliente: ");
+        float peso_prato = teclado.nextFloat();
+
+        System.out.println("Valor total do prato: R$ " + ((peso_prato - 0.750) * 25));
+
+        teclado.close();
+    }
+}
