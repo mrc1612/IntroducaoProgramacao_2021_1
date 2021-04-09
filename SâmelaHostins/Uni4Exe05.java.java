7d119b7565f0a7af76f1e57fbb92e3277b591188
Sâmela Hostins <samela@hostins.com.br>
2021-04-09 20:41:18 -0300

    Add files via upload

---
+++ SâmelaHostins/Uni4Exe05.java.java
@@ -0,0 +1,15 @@
+import java.util.Scanner;
+
+public class App {
+    public static void main(String[] args) throws Exception {
+        Scanner ler = new Scanner(System.in);
+        boolean cor;
+        System.out.println("A cor é azul? Responda com true ou false");
+cor = ler.nextBoolean();
+if (cor == true) {
+    System.out.println("Sim");
+}else{
+    System.out.println("Não");
+}
+}
+    }
