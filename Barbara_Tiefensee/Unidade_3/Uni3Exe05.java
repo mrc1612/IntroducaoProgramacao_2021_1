3cb25ff5dbff4dc7b4ca274c95048087101783e9
BarbaraTiefensee <60518782+BarbaraTiefensee@users.noreply.github.com>
2021-04-11 11:23:38 -0300

    Add files via upload

---
+++ Barbara_Tiefensee/Unidade_3/Uni3Exe05.java
@@ -0,0 +1,20 @@
+import java.util.*;
+
+public class Uni3Exe05 {
+    public static void main(String[] args)
+    {
+        Scanner teclado = new Scanner(System.in);
+
+        int frango;
+        double totalGranja;
+        double anelComum = 4.00;
+        double anelAlimento = 3.50 * 2;
+
+        System.out.println("Quantidade de frango: ");
+        frango = teclado.nextInt();
+
+        totalGranja = frango * (anelComum + anelAlimento);
+
+        System.out.println("Total da granja: R$" + totalGranja);
+    }
+}
