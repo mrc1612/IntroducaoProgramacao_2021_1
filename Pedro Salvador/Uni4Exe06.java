fe0b054197203b1f02f49f41b11e891f9957cea1
PedroPFS <81395668+PedroPFS@users.noreply.github.com>
2021-04-14 21:29:49 -0300

    Create Uni4Exe06.java

---
+++ Pedro Salvador/Uni4Exe06.java
@@ -0,0 +1,36 @@
+import java.util.Scanner;
+
+public class App {
+    public static void main(String[] args) throws Exception {
+
+        Scanner ler = new Scanner(System.in);
+
+char d;
+
+System.out.println("Como você se define? Escreva em letra maiuscula, M =  Masculino, F = Feminino ou I = Não Informado.");
+
+d = ler.next().charAt(0);
+
+if (d =='M' ) {
+
+System.out.println("Masculino.");
+
+} else {
+
+if (d == 'F') {
+
+System.out.println("Feminino.");
+
+} else {
+
+if (d == 'I') {
+
+System.out.println("Não Informado.");
+
+} else {
+
+System.out.println("Entrada Incorreta.");
+
+    }
+
+}}}}
