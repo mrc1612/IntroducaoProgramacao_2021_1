5ddaecea9b8642b92e9c75b27906e63006a93a42
ElizabethJunkes <81395539+ElizabethJunkes@users.noreply.github.com>
2021-04-15 00:55:31 -0300

    Update Uni4Ex20.java

--- ElizabethJunkes/Uni4Ex20.java
+++ ElizabethJunkes/Uni4Ex20.java
@@ -5,36 +5,36 @@ public class Uni4Ex20 {
     public static void main(String[] args){
     Scanner teclado = new Scanner(System.in);
     float notaProva1, notaProva2, notaProva3, notaExercicios;
-        System.out.print("Informe a nota da Prova 1: ");
+        System.out.print("Informe a nota da primeira prova: ");
         notaProva1 = teclado.nextFloat();
-        System.out.print("Informe a nota da Prova 2: ");
+        System.out.print("Informe a nota da segunda prova: ");
         notaProva2 = teclado.nextFloat();
-        System.out.print("Informe a nota da Prova 3: ");
+        System.out.print("Informe a nota da terceira prova: ");
         notaProva3 = teclado.nextFloat();
-        System.out.print("Informe a média dos exercicios: ");
+        System.out.print("Informe a média dos exercícios: ");
         notaExercicios = teclado.nextFloat();
         String conceito = " e o conceito do aluno é ";
         String mediaAluno = "A média do aluno é de ";
-        String aprovado = "Situacao do aluno: APROVADO";
+        String aprovado = "Situação do aluno: aprovado!";
         float media = (float) ((notaProva1 + notaProva2 * 2 + notaProva3 * 3 +notaExercicios) / 7);
         DecimalFormat df = new DecimalFormat("#.#");
-        if(media >= 9){
+        if (media >= 9) {
             System.out.println(mediaAluno+ df.format(media) +conceito + "A.");
             System.out.println(aprovado);
-        }else{
-            if(media >= 7.5 && media < 9){
+        }else {
+            if(media >= 7.5 && media < 9) {
             System.out.println(mediaAluno+ df.format(media) +conceito + "B.");
             System.out.println(aprovado);
-        }else{
-         if(media >= 6 && media < 7.5){
+        }else {
+         if (media >= 6 && media < 7.5) {
             System.out.println(mediaAluno+ df.format(media) +conceito + "C.");
             System.out.println(aprovado);
-        }else{
-         if(media >= 4.0 && media < 6){
+        }else {
+         if(media >= 4.0 && media < 6) {
             System.out.println(mediaAluno+ df.format(media) +conceito + "D.");
             System.out.println("Situação do aluno: reprovado!");
-        }else{
-        if (media < 4){
+        }else {
+        if (media < 4) {
             System.out.println(mediaAluno+ df.format(media) +conceito + "E.");
             System.out.println("Situação do aluno: reprovado!");
 }}}}}}}
