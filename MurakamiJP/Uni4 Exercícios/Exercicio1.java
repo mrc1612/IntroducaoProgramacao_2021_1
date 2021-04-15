77b8e4d496ca71720284b2db5817fb8bde5d1ce6
MurakamiJP <56041365+MurakamiJP@users.noreply.github.com>
2021-04-15 18:23:28 -0300

    Update Exercicio1.java

--- MurakamiJP/Uni4 Exercícios/Exercicio1.java
+++ MurakamiJP/Uni4 Exercícios/Exercicio1.java
@@ -2,24 +2,27 @@ import java.util.Scanner;
 
 public class Exercicio1 {
     public static void main(String[] args) {
+        
         Scanner scanner = new Scanner(System.in);
 
-        double horasTrabalhadasMes = scanner.nextDouble();
-        double valorPorHora = scanner.nextDouble();
+        double horas_mes = scanner.nextDouble();
+        double ganhos_hora = scanner.nextDouble();
 
-        double horasSemanais = horasTrabalhadasMes / 4;
+        double horas_sem = horas_mes / 4;
 
         double salario = 0;
 
-        if( horasSemanais > 40){
-            double horaExtra = (horasTrabalhadasMes - 160) * ((valorPorHora * 0.5) + valorPorHora);
-            salario = valorPorHora *horasTrabalhadasMes + horaExtra;
-            System.out.println("salario total: " + salario);
+        if( horas_sem > 40){
+            
+            double ganhos_hora_extra = (horas_mes - 160) * ((ganhos_hora * 0.5) + ganhos_hora);
+            salario = ganhos_hora * horas_mes + ganhos_hora_extra;
+            System.out.println("O salario total será: " + salario);
             scanner.close();
         }
         else{
-            salario = valorPorHora * horasTrabalhadasMes;
-            System.out.println("salario total: " + salario);
+            
+            salario = ganhos_hora * horas_mes;
+            System.out.println("O salario total será: " + salario);
             scanner.close();
         }
     }
