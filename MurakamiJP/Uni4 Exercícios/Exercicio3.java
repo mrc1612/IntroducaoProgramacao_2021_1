16fa7bf911e372b9aa19d06b7228644595906ea0
MurakamiJP <56041365+MurakamiJP@users.noreply.github.com>
2021-04-15 18:26:05 -0300

    Update Exercicio3.java

--- MurakamiJP/Uni4 Exercícios/Exercicio3.java
+++ MurakamiJP/Uni4 Exercícios/Exercicio3.java
@@ -5,17 +5,19 @@ public class Exercicio3 {
 
         Scanner teclado = new Scanner (System.in);
 
-        int numero1, numero2;
+        int n1, n2;
 
-        System.out.println("Informe o número 1: ");
+        System.out.println("Informe o primeiro número: ");
         numero1 = teclado.nextInt();
-        System.out.println("Informe o número 2: ");
+        
+        System.out.println("Informe o segundo número: ");
         numero2 = teclado.nextInt();
 
-        if (numero1>numero2) {
-            System.out.println("O número 1 = "+numero1+", é maior que o número 2 = "+numero2+"");
+        if(n1>n2) {
+            System.out.println("O número 1 é maior que o número 2);
+        
         }else{
-            System.out.println("O número 1 = "+numero2+", é maior que o número 2 = "+numero1+"");
+            System.out.println("O número 1 é menor que o número 2);
         }
         
         teclado.close();
