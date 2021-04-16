aae873b92a657eeaf6a9fb361f10e8e71e781456
PedroPFS <81395668+PedroPFS@users.noreply.github.com>
2021-04-16 18:22:56 -0300

    Create Uni4Exe08.java

---
+++ Pedro Salvador/Uni4Exe08.java
@@ -0,0 +1,15 @@
+import java.util.Scanner;
+public class App {
+    public static void main(String[] args) throws Exception {
+        Scanner ler = new Scanner(System.in);
+        char letra;
+        System.out.println("Informe uma letra em minuscula");
+        letra= ler.next().charAt(0);
+        if (letra == 'a'|| letra =='e' || letra =='i' || letra =='o' || letra == 'u') {
+            System.out.println("A letra " + letra + " é vogal");
+        }else{
+            System.out.println("A letra " + letra + " é  consoante");
+        }
+
+    }
+}
