7f088b1660c65f4c3f060bbe1b42e170873646a8
Sâmela Hostins <samela@hostins.com.br>
2021-05-07 12:02:31 -0300

    Add files via upload

---
+++ SâmelaHostins/Unidade5/Uni5Exe05.java
@@ -0,0 +1,11 @@
+public class Uni5Exe05 {
+    public static void main(String[] args) throws Exception {
+        /*Descreva um algoritmo que escreva os n primeiros termos da seguinte sequência de números:
+8, 10, 16, 18, 32, 34, 64...
+O valor n deve ser lido e deve ser maior do que 2.
+Isso é PROGRESSÃO GEOMÉTRICA.*/
+    for(int i=4; i< 150; i=i*2){
+        System.out.println("A sequencia é: " + i);
+    }
+    }
+}
