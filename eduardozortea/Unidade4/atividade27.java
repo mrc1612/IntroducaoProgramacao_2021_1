b8c8f55124a2473e9eb53f10765f20541c669ec2
EduardoZortea18 <60518656+EduardoZortea18@users.noreply.github.com>
2021-05-17 18:34:40 -0300

    test

--- eduardozortea/Unidade4/atividade27.java
+++ eduardozortea/Unidade4/atividade27.java
@@ -1,7 +1,18 @@
 package eduardozortea.Unidade4;
 
+import java.util.Scanner;
+
 public class atividade27 {
     public static void main(String[] args) {
+        Scanner scanner = new Scanner(System.in);
         
+        int horasChegada = scanner.nextInt();
+        int minutosChegada = scanner.nextInt();
+
+        int horasSaida = scanner.nextInt();
+        int minutosSaida = scanner.nextInt();
+
+        
+
     }
 }
