6a78ade5b5a7fc1171dae774ed39bfcce82fd7bc
ElizabethJunkes <81395539+ElizabethJunkes@users.noreply.github.com>
2021-05-17 16:46:41 -0300

    Add files via upload

--- ElizabethJunkes/Uni5Ex16.java
+++ ElizabethJunkes/Uni5Ex16.java
@@ -1,21 +1,30 @@
-/*
- * To change this license header, choose License Headers in Project Properties.
- * To change this template file, choose Tools | Templates
- * and open the template in the editor.
- */
-package uni5ex16;
-
-/**
- *
- * @author eliza
- */
+import java.util.Scanner;
 public class Uni5Ex16 {
-
-    /**
-     * @param args the command line arguments
-     */
-    public static void main(String[] args) {
-        // TODO code application logic here
+    public static void main(String[] args) throws Exception {
+        Scanner teclado = new Scanner(System.in);
+        int masculino =0, feminino = 0;
+        String sexo;
+        double altura = 1; double alturaTotalF =0, alturaTotalM =0; double mediaAlturaFem = 0, mediaAlturaGrupo =0;
+        while (altura != 0) {
+            System.out.print("Informe a sua altura: ");
+            altura = teclado.nextDouble();
+            if (altura == 0){
+            break;
+            }
+            System.out.print("Informe o seu sexo: M para Masculino e F para feminino ");
+            sexo = teclado.next();
+        if (sexo.equals("M") || sexo.equals("m")) {
+           masculino ++;
+           alturaTotalM += altura;
+        }
+        if (sexo.equals("F") || sexo.equals("f") ){
+            feminino ++;
+            alturaTotalF += altura;
+                }    
+            }
+            mediaAlturaFem = alturaTotalF / feminino;
+            mediaAlturaGrupo = (alturaTotalM + alturaTotalF) / (masculino + feminino);
+        System.out.println("A média da altura das mulheres é de: " + mediaAlturaFem);
+        System.out.println("A média da altura total do grupo é de: " + mediaAlturaGrupo);
     }
-    
-}
+}    
