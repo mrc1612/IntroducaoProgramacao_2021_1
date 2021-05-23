6162791447afe41d41b75a70c3427bfd0741addf
cmbcosta <80531710+cmbcosta@users.noreply.github.com>
2021-05-23 16:14:50 -0300

    feat:

--- cmbcosta/Unidade 6/Uni6Exe04.java
+++ cmbcosta/Unidade 6/Uni6Exe04.java
@@ -7,8 +7,9 @@ public class Uni6Exe04 {
         int[] array1 = new int[10];
         int[] arraySum = new int[10];
         readyValueArray(array, array1);
-        writeArray(array, array1);
         sumArray(array, array1, arraySum);
+        writeArray(array, array1, arraySum, 0);
+
     }
 
     // READY
@@ -26,25 +27,26 @@ public class Uni6Exe04 {
             scanner.close();
         }
     public void sumArray(int[] array, int[] array1, int[] arraySum) {    
+        for (int i = 0; i < arraySum.length; i++) {
+            
+            arraySum[i] = array[i] + array1[i];
+
+
+        }
+	}
+    
+   public void writeArray(int[] array, int[] array1, int[] arraySum, int sumArray) {
         for (int i = 0; i < array.length; i++) {
-        float sum = array[i];
+            System.out.println("array[" + i + "]: " + array[i]);
 
         }
     
-        for (int j = 0; j < array1.length; j++) {
-            float sum1 = array[j];
-        
-    }
-}
-    
-   public void writeArray(int[] array, int[] array1) {
-        for (int i = 0; i < array.length; i++) {
-            //System.out.println("array[" + i + "]: " + array[i]);
+        for (int i = 0; i < array1.length; i++) {
+            System.out.println("array1[" + i + "]: " + array1[i]);
 
         }
-    
-        for (int j = 0; j < array1.length; j++) {
-            //System.out.println("array1[" + j + "]: " + array1[j]);
+        for (int i = 0; i < arraySum.length; i++) {
+            System.out.println("arraySum[" + i + "]: " + arraySum[i]);
 
         }
     }
