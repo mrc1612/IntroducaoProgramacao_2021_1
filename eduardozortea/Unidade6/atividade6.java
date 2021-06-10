1a22029e8b434449a2216a4c3cbfdcfd09dfb530
Eduardo Zortea <60518656+EduardoZortea18@users.noreply.github.com>
2021-06-10 20:51:54 -0300

    Create atividade6.java

---
+++ eduardozortea/Unidade6/atividade6.java
@@ -0,0 +1,57 @@
+import java.util.Scanner;
+
+public class Uni6Exe6 {
+     public static void main(String[] args) {
+        new Uni6Exe6();
+    }
+  
+    public Uni6Exe6() {
+
+        Scanner scanner = new Scanner(System.in);
+
+        float numeros[] = readVector();
+        boolean found = research(numeros);
+
+        if (found) {
+          
+            System.out.println("Número existe");
+          
+        } 
+        else {
+          
+            System.out.println("Número não existe");
+          
+        }
+
+        scanner.close();
+    }
+
+    public float[] readVector() {
+
+        System.out.print("Informe o tamanho do vetor desejado: "); // Valor "N"
+        int tamanho = s.nextInt();
+
+        float[] vetor = new float[tamanho];
+
+        for (int indice = 0; indice < tamanho; indice++) {
+            System.out.print("Digite um valor real: ");
+            vetor[indice] = scanner.nextFloat();
+        }
+
+        return vetor;
+    }
+
+    public boolean research(float[] vetor) {
+      
+        System.out.print("Digite um número: ");
+        float numero = scanner.nextFloat();
+
+        for (int i = 0; i< vetor.length; i++) {
+            if (vetor[i] == numero) {
+                return true;
+            }
+        }
+
+        return false;
+    }
+}
